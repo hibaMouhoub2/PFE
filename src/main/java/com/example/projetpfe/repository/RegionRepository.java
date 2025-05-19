cc7979e10c6d792d1fcc9aeb26b475beee461cd2
hibaMouhoub2 <hiba3005khalid@gmail.com>
2025-05-19 12:02:01 +0100

    test erreur rapport

--- src/main/java/com/example/projetpfe/repository/RegionRepository.java
+++ src/main/java/com/example/projetpfe/repository/RegionRepository.java
@@ -1,8 +1,12 @@
 package com.example.projetpfe.repository;
 
+import com.example.projetpfe.entity.Direction;
 import com.example.projetpfe.entity.Region;
+import com.example.projetpfe.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
@@ -10,4 +14,5 @@ public interface RegionRepository extends JpaRepository<Region, Long> {
     Region findByCode(String code);
     boolean existsByName(String name);
     boolean existsByCode(String code);
+    List<Region> findByDirection(Direction direction);
 }
